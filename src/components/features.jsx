7553a3a8dd5f25e82e9068b932d0502be3269c13
sobindra2005 <sobhindra2005@gmail.com>
2025-01-06 16:42:40 +0545

    refactor: enhance layout and responsiveness of Features section for improved user experience

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -90,22 +90,24 @@ const Features = async () => {
   // }, []);
 
   return (
-    <section id="Service&facilities" className="py-16 text-black bg-gray-100">
-      <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold text-center mb-6">Why Choose FitLife?</h2>
-        <p className="text-gray-600 w-full text-center mb-12">
-          {" Fit Life gym  for personalized fitness, expert guidance, and flexible coaching"}
-        </p>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          {Response.map((feature, index) => (
-            <div key={index} className="bg-white p-6 rounded-lg shadow-md">
-              <div className="flex items-center justify-center mb-4">
-                <Image src={`${imageUrlBuilder({ projectId, dataset }).image(feature.icon)?.width(20).height(20).url()}`} width={20} height={20} alt="icon" className="h-10 w-10" />
+    <section id="Service&facilities" className="py-16 text-black bg-gray-100 ">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold text-center mb-6">Why Choose FitLife?</h2>
+          <p className="text-gray-600 w-full text-center mb-12">
+            {" Fit Life gym  for personalized fitness, expert guidance, and flexible coaching"}
+          </p>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            {Response.map((feature, index) => (
+              <div key={index} className="bg-white p-6 rounded-lg shadow-md">
+                <div className="flex items-center justify-center mb-4">
+                  <Image src={`${imageUrlBuilder({ projectId, dataset }).image(feature.icon)?.width(20).height(20).url()}`} width={20} height={20} alt="icon" className="h-10 w-10" />
+                </div>
+                <h3 className="text-xl font-semibold mb-2 text-center">{feature.title}</h3>
+                <p className="text-gray-600 text-center">{feature.description}</p>
               </div>
-              <h3 className="text-xl font-semibold mb-2 text-center">{feature.title}</h3>
-              <p className="text-gray-600 text-center">{feature.description}</p>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </section>
